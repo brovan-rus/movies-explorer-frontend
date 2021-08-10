cd1a88b9862e26035c91dc51c3368ea033043ca9
Konstantin Brovtsev <brovan@yandex.ru>
2021-08-11 00:33:01 +0300

    feat: add aboutProject section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
     <main className="landing">
       <Promo />
       <NavTab />
+      <AboutProject />
     </main>
   );
 }
--- src/styles/mixins/index.js
+++ src/styles/mixins/index.js
@@ -2,8 +2,6 @@ const { resEquation } = require('../../utils/utils');
 
 const mixins = {
   responsiveValue(mixin, param, resA, resB, sizeA, sizeB) {
-    const resANum = resA * 1;
-    const resBNum = resB * 1;
     const { shift, factor } = resEquation(resA, resB, sizeA, sizeB);
     return {
       [`${param}`]: `calc(${shift}px + 100vw * ${factor})`,
