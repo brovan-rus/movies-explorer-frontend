843d6073523d7c4ee716b8cafc458790f5c3c02a
Konstantin Brovtsev <brovan@yandex.ru>
2021-08-12 00:46:08 +0300

    feat: add portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,19 +1,27 @@
 function Portfolio() {
   return (
     <section className="portfolio">
-      <h2 className="landing__section-title landing__section-title_place_portfolio">Портфолио</h2>
+      <h2 className="landing__section-title landing__section-title_place_portfolio app__text">
+        Портфолио
+      </h2>
       <ul className="portfolio__list">
-        <li className="portfolio__list-element">
-          <span className="porfolio__project-name app__text">Статичный сайт</span>
-          <a href="" className="portfolio__link app__link"></a>
+        <li className="portfolio__list-element app__text">
+          <span>Статичный сайт</span>
+          <a href="" className="portfolio__link app__link">
+            ↗
+          </a>
         </li>
-        <li className="portfolio__list-element">
-          <span className="porfolio__project-name app__text">Адаптивный сайт</span>
-          <a href="" className="portfolio link app__link"></a>
+        <li className="portfolio__list-element app__text">
+          <span>Адаптивный сайт</span>
+          <a href="" className="portfolio__link app__link">
+            ↗
+          </a>
         </li>
-        <li className="portfolio__list-element">
-          <span className="porfolio__project-name app__text">Одностраничное приложение</span>
-          <a href="" className="portfolio__link app__link"></a>
+        <li className="portfolio__list-element app__text">
+          <span>Одностраничное приложение</span>
+          <a href="" className="portfolio__link app__link">
+            ↗
+          </a>
         </li>
       </ul>
     </section>
